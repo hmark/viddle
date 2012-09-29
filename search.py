85ba4d06f292c4e1d66fcae81c3fe934757fc14b
hlavko <hlavino@gmail.com>
2012-09-29 15:09:40 +0200

    moved search.py to cherrypy layer

--- search.py
+++ search.py
@@ -1,21 +1,24 @@
-import index
+import vidcr.index
 from whoosh.qparser import MultifieldParser
 
 class Query:
 
 	def search_term(self, term):
-		self.whoosh = index.Whoosh()
+		self.whoosh = vidcr.index.Whoosh()
 		parser = MultifieldParser(["title", "body"], schema=self.whoosh.index.schema)
 		query = parser.parse(term)
 
 		with self.whoosh.index.searcher() as s:
 			results = s.search(query)
+			roll = str(results.top_n) + "<br>"
 			print(results.top_n)
 			for result in results:
 				#print(result.highlights("title"))
-				print(result)
-				print(result["title"])
+				#roll += str(result)
+				#roll += "<br>"
+				#print(result)
+				roll += "<a href='" + str(result["url"]) + "'>" + str(result["title"]) + "</a><br>"
+				#print(result["title"])
 				#print(result["body"])
 
-query = Query()
-query.search_term("hlina")
+		return roll
