f169c483d911ca5790f10ef6a4964b245bfe731c
hlavko <hlavino@gmail.com>
2012-10-10 16:05:44 +0200

    added pagination

--- search/site.py
+++ search/site.py
@@ -4,10 +4,12 @@ import search
 from mako.template import Template
 from mako.lookup import TemplateLookup
 import os.path
+import math
 
 dirname = os.path.dirname(__file__)
-#CONFIG = dirname + '/conf/dev.conf'
-CONFIG = dirname + '/conf/prod.conf'
+
+CONFIG = dirname + '/conf/dev.conf'
+#CONFIG = dirname + '/conf/prod.conf'
 
 tmpl_lookup = TemplateLookup(directories=[dirname + '/templates'], module_directory=dirname + '/tmp/mako_modules', input_encoding='utf-8', output_encoding='utf-8')
 
@@ -20,16 +22,18 @@ class RootPage(object):
 		
 		return tmpl.render_unicode(term=None, data=None, length=-1, newest=query.get_newest(5), help=False)
 
-	def search(self, term=None):
+	def search(self, term=None, page=1):
 		tmpl = tmpl_lookup.get_template("index.html")
 		
-		length = 0
+		results_num = 0
+		pages = 0
+		page = int(page)
 		if term != None:
 			query = search.Query()
-			data = query.search_term(term)
-			length = len(data)
+			data, results_num = query.search_term(term, page)
+			pages = math.ceil(results_num / 5)
 
-		return tmpl.render_unicode(term=term, data=data, length=length, newest=query.get_newest(5), help=False)
+		return tmpl.render_unicode(term=term, data=data, length=results_num, newest=query.get_newest(5), help=False, page=page, pages=pages)
 
 	def help(self):
 		tmpl = tmpl_lookup.get_template("index.html")
