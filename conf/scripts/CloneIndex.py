5fc92ef93e2f57eae1cf5f8b951ebb813cd83c42
hlavko <hlavino@gmail.com>
2012-10-15 15:58:55 +0200

    updated CloneIndex script

--- conf/scripts/CloneIndex.py
+++ conf/scripts/CloneIndex.py
@@ -14,15 +14,29 @@ def get_player(video):
 	elif re.match(r"^http://video.ted.com", video):
 		return "ted.com(old)"
 
-pathname = os.path.dirname(__file__) + "/../../data/index2"
+pathname = "../../data/index2"
 schema = Schema(url=ID(stored=True, unique=True), video=ID(stored=True, unique=True), player=TEXT(stored=True), name=TEXT(stored=True), title=TEXT(stored=True, field_boost=5.0), body=TEXT(vector=True, stored=True))
 os.mkdir(pathname)
 newIndex = create_in(pathname, schema)
 newWriter = newIndex.writer()
 
-oldIndex = open_dir(os.path.dirname(__file__) + "/../../data/index")
+oldIndex = open_dir("../../data/index")
 oldWriter = oldIndex.writer()
 oldSearcher = oldIndex.searcher()
+
+# get document id by URL
+#id1 = oldSearcher.document_number(url="http://tv.sme.sk/v/25315/ucitelia-zavru-skoly-od-26-novembra.html")
+#id2 = oldSearcher.document_number(url="http://tv.sme.sk/v/25314/skolski-zamestnanci-budu-strajkovat.html")
+#print(id1)
+#print(id2)
+
+# delete document by ID
+#oldWriter.delete_document(id2)
+#oldWriter.delete_document(id1)
+#oldWriter.commit()
+
+#for doc in oldSearcher.reader().all_stored_fields():
+#	print(doc["player"], doc["url"])
 
 for doc in oldSearcher.reader().all_stored_fields():
 	player = get_player(doc["video"][0])
@@ -32,5 +46,3 @@ newWriter.commit()
 newSearcher = newIndex.searcher()
 for doc in newSearcher.reader().all_stored_fields():
 	print(doc)
-
-
