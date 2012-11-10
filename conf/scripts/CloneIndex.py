97ef0bdbe0a21ed583157220d56eebfad2a4e261
hlavko <hlavino@gmail.com>
2012-11-10 13:13:39 +0100

    added testing script

--- conf/scripts/CloneIndex.py
+++ conf/scripts/CloneIndex.py
@@ -2,18 +2,8 @@ import os
 import re
 from whoosh.fields import Schema, TEXT, ID, STORED
 from whoosh.index import create_in, open_dir
-
-def get_player(video):
-	import re
-
-	if re.match(r"^http://i.sme.sk/datamm", video):
-		return "sme.sk"
-	elif re.match(r"^http://www.youtube.com/embed", video):
-		return "youtube.com"
-	elif re.match(r"^http://embed.ted.com/talks", video):
-		return "ted.com"
-	elif re.match(r"^http://video.ted.com", video):
-		return "ted.com(old)"
+from whoosh.qparser import MultifieldParser
+from whoosh.query import Every
 
 #pathname = "../../data/index2"
 #schema = Schema(url=ID(stored=True, unique=True), video=ID(stored=True, unique=True), player=TEXT(stored=True), name=TEXT(stored=True), title=TEXT(stored=True, field_boost=5.0), body=TEXT(vector=True, stored=True))
@@ -21,20 +11,31 @@ def get_player(video):
 #newIndex = create_in(pathname, schema)
 #newWriter = newIndex.writer()
 
-oldIndex = open_dir("../../data/index")
-oldWriter = oldIndex.writer()
-oldSearcher = oldIndex.searcher()
+index = open_dir("../../data/index")
+searcher = index.searcher()
 
-# get document id by URL
-id1 = oldSearcher.document_number(url="http://edition.cnn.com/2012/11/03/us/tropical-weather-sandy/index.html")
-id3 = oldSearcher.document_number(url="http://edition.cnn.com/2012/11/02/world/europe/turkey-prisons-hunger-strike/index.html")
-print(id1, id3)
+parser = MultifieldParser(["title", "body"], schema=index.schema)
+query = parser.parse("doprava")
+#query = Every()
+results_len = 0
+
+with searcher as s:
+	results = s.search(query, limit=None)
+
+	for result in results:
+		if result.score > 0:
+			print(str(result["title"]))
+			print(str(result["url"]))
+			print("---------------")
+			results_len += 1
+
+print("NUMBER OF RESULTS:", results_len)
 
 # delete document by ID
-oldWriter.delete_document(id3)
+#oldWriter.delete_document(id3)
 #oldWriter.delete_document(id2)
-oldWriter.delete_document(id1)
-oldWriter.commit()
+#oldWriter.delete_document(id1)
+#oldWriter.commit()
 
 #for doc in oldSearcher.reader().all_stored_fields():
 	#if re.search(r"\?hpt", doc["url"]):
